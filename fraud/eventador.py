2143be0c7042e9389baa5f18dbe50ffa0ea34c08
Kenny Gorman <kgorman@eventador.io>
2020-06-03 14:43:01 -0500

    changes to use rest api

---
+++ fraud/eventador.py
@@ -0,0 +1,57 @@
+import requests
+import uuid
+import os
+import sys
+
+TOPIC      = os.getenv("TOPIC", "authorizations")
+BASE_URL   = os.getenv("URL")
+CREATE_URL = "{}/kafka/topic/create".format(BASE_URL)
+SEND_URL   = "{}/kafka/topic/{}/send".format(BASE_URL, TOPIC)
+API_KEY    = os.getenv("API_KEY")
+
+## Status Codes
+TOPIC_ALREADY_EXISTS = 409
+
+def create_session():
+    session = requests.Session()
+    session.headers.update({
+        "Authorization" : "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+    })
+    return session
+
+def create_topic(session):
+    try:
+        response = session.post(CREATE_URL, json = {
+            "topic" : TOPIC,
+            "partitions": 1,
+            "replicas": 1
+        })
+        response.raise_for_status()
+        return response
+    except requests.exceptions.HTTPError as e:
+        status_code = e.response.status_code
+        if status_code == TOPIC_ALREADY_EXISTS:
+            print("{}-topic already exists, continuing normally".format(status_code))
+        else:
+            print("{}-an unknown error occured".format(status_code))
+
+def createRequestBody(d):
+    body = {
+        "record": {
+            "key" : str(uuid.uuid4()),
+            "value" : d
+        }
+    }
+    return body
+
+def produce(data):
+    try:
+        session  = create_session()
+        response = create_topic(session)
+        requestBody = createRequestBody(data)
+        response    = session.post(SEND_URL, json = requestBody)
+        response.raise_for_status()
+        print("{}-{}\n{}".format(response.status_code, data, response.json()))
+    except Exception as e:
+        print(e)
--- fraud/produce_fraud.py
+++ fraud/produce_fraud.py
@@ -8,16 +8,10 @@ import random
 from random import uniform
 import time
 import sys
+import eventador as ev
 
 from card_generator import generate_card
 from geopoint import create_geopoint
-from confluent_kafka import Consumer, KafkaError, Producer
-from confluent_kafka.admin import AdminClient, NewTopic
-
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "fraud")
-KAFKA_BROKERS = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
-SASL_USERNAME = os.getenv("SASL_USERNAME", "Nologin")
-SASL_PASSWORD = os.getenv("SASL_PASSWORD", "NoPass")
 
 CITIES = [
     {"lat": 30.2672, "lon": -97.7430608, "city": "austin"},
@@ -25,18 +19,6 @@ CITIES = [
     {"lat": 39.7392, "lon": -104.9903, "city": "denver"},
     {"lat": 32.7767, "lon": -96.7970, "city": "dallas"}
 ]
-
-
-def make_topic(t, c):
-    a = AdminClient(c)
-    topics = [KAFKA_TOPIC]
-    fs = a.create_topics([NewTopic(topic, num_partitions=3, replication_factor=3) for topic in topics])
-    for topic, f in fs.items():
-        try:
-            f.result()  # The result itself is None
-            print("Topic {} created".format(topic))
-        except Exception as e:
-            print("Failed to create topic, it may exist already {}: {}".format(topic, e))
 
 def get_latlon():
     geo = random.choice(CITIES)
@@ -61,52 +43,27 @@ def make_fraud(seed, card, user, latlon):
               }
     return payload
 
-def delivery_report(err, msg):
-    """ Called once for each message produced to indicate delivery result.
-        Triggered by poll() or flush(). """
-    if err is not None:
-        print('Message delivery failed: {}'.format(err))
-    else:
-        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
-
 def main():
-
-    conf = {
-      'bootstrap.servers': KAFKA_BROKERS,
-      'api.version.request': True,
-      'broker.version.fallback': '0.10.0.0',
-      'api.version.fallback.ms': 0,
-      'sasl.mechanisms': 'PLAIN',
-      'security.protocol': 'SASL_SSL',
-      'sasl.username': SASL_USERNAME,
-      'sasl.password': SASL_PASSWORD,
-      'group.id': 'fraud_group',
-      'default.topic.config': {'auto.offset.reset': 'smallest'}
-    }
-    producer = Producer(**conf)
-    print("connected to {} topic {}".format(KAFKA_BROKERS, KAFKA_TOPIC))
-    make_topic(KAFKA_TOPIC, conf)
 
     payload = {}
     fraud_trigger = 15
     i = 1
 
     while True:
-        producer.poll(0)
 
         if i % fraud_trigger == 0:
-            print("making fraud")
+            # fraud
             card = generate_card("visa16")
             user = get_user()
             latlon = get_latlon()
             for r in range(3):
                 payload = make_fraud(r, card, user, latlon)
-                print(payload)
                 try:
-                    producer.produce(KAFKA_TOPIC, json.dumps(payload).rstrip().encode('utf8'), callback=delivery_report)
+                    ev.produce(payload)
                 except Exception as ex:
                     print("unable to produce {}".format(ex))
         else:
+            # not fraud
             latlon = get_latlon()
             payload = {
                 "userid": get_user(),
@@ -117,11 +74,10 @@ def main():
             }
 
         try:
-            producer.produce(KAFKA_TOPIC, json.dumps(payload).rstrip().encode('utf8'), callback=delivery_report)
+            ev.produce(payload)
         except Exception as ex:
             print("unable to produce {}".format(ex))
-        print(payload)
-        producer.flush()
+
         i += 1
 
 if __name__== "__main__":
